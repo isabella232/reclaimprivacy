ffc8f04f7771ba7019296c2808096fcfc7951ab8
Peter Burns <peter@metaweb.com>
2010-05-31 01:16:15 -0700

    Improved reliability when navigating around Facebook with AJAX

--- chrome_extension/inject.js
+++ chrome_extension/inject.js
@@ -6,4 +6,16 @@ var toInject = [
   "</div>"  
 ].join('');
 
-$("#pagelet_status_updater").append(toInject);      
+function inject() {
+  if ($("#reclaimPrivacyButton").length === 0)
+    $("#pagelet_status_updater").append(toInject);
+}
+
+inject();
+
+//Facebook does navigation by AJAX, so need to check back periodically
+//however, I can't get access to window.location.hash or window.onhashchange
+//reliably, so this is the best I've got for now
+setInterval(function() {
+  inject();
+}, 500);
