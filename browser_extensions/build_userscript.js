688056f90b90b23d962fc234748f08d672615906
Peter Burns <peter@metaweb.com>
2010-06-09 00:22:11 -0700

    Move all browser extension stuff to its own directory

--- browser_extensions/build_userscript.js
+++ browser_extensions/build_userscript.js
@@ -3,26 +3,26 @@
 //
 //usage: node build_userscript.js
 //
-//output: writes a file: reclaimprivacy.user.js
+//output: writes a file: userscript/reclaimprivacy.user.js
 
 var fs = require("fs");
 var sys = require("sys")
 
-var source = fs.readFileSync("../chrome_extension/inject.js", "utf-8");
-var manifest = readJSONFile("../chrome_extension/manifest.json");
+var source = fs.readFileSync("chrome_extension/inject.js", "utf-8");
+var manifest = readJSONFile("chrome_extension/manifest.json");
 var headers = constructHeaders(manifest);
 
 var localization_code = fs.readFileSync("getMessage.js", "utf-8");
-var localization_map = constructLocalizationMap("../chrome_extension/_locales");
+var localization_map = constructLocalizationMap("chrome_extension/_locales");
 localization_code = localization_code.replace("var messages = {}", "var messages = " + (JSON.stringify(localization_map, null, 2)));
 
-var styleFiles = manifest.content_scripts.map(function(content_script){return "../chrome_extension/" + content_script.css});
+var styleFiles = manifest.content_scripts.map(function(content_script){return "chrome_extension/" + content_script.css});
 var style_injector = constructStyleInjector(styleFiles);
 
 source = source.replace("chrome.i18n.getMessage", "getMessage");
 
 var finalFile = [headers,style_injector,localization_code,source].join("\n\n\n");
-fs.writeFile("reclaimprivacy.user.js", finalFile);
+fs.writeFile("userscript/reclaimprivacy.user.js", finalFile);
 
 
 //construct the localization map from the files in _locales
